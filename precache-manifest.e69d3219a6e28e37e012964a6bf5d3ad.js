1a809ad0cb730a3464cb4c29e436491b6e622074
insooooooooo <oasisholics@gmail.com>
2020-09-23 23:06:57 +0900

    Updates

---
+++ precache-manifest.e69d3219a6e28e37e012964a6bf5d3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47278be791dd9e230120275c3e2383e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cf6e43b99d0eed5fe57f",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6e43b99d0eed5fe57f",
+    "url": "/movie_app/static/js/main.a7730793.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
