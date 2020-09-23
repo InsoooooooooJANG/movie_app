0e2aa36db28bf44d15992277a861ee98bb52a3de
Insoo Jang <jangis@minisoft.co.kr>
2020-09-23 13:37:49 +0900

    second study

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Axios from "axios";
+import Movie from "./Movie";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
@@ -7,16 +9,21 @@ class App extends React.Component{
     movies:[]
   };
 
+  getMovies= async () => {
+    const {data:{data:{movies}}} = await Axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading:false});
+  }
+
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false});
-    }, 6000)
+    this.getMovies();
   }
 
   render(){
-    const {isLoading} = this.state;
+    const {isLoading, movies}  = this.state;
     return (
-      <div>{isLoading? "Loading":"We are ready"}</div>
+      <div>{isLoading? "Loading": movies.map(movie=>{
+        return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.midium_cover_image}></Movie>
+      })}</div>
     )
   }
   
